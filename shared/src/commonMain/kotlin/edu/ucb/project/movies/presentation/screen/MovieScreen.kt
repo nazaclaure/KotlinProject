d90b7fcd203dd8908ba704787816524743ed5e33
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-08-24 18:04:26 -0400

    feat: interfaz inicial de MovieScreen con FlowRow

---
+++ shared/src/commonMain/kotlin/edu/ucb/project/movies/presentation/screen/MovieScreen.kt
@@ -0,0 +1,39 @@
+package edu.ucb.project.movies.presentation.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun MoviesScreen() {
+    val rows = 3
+    val columns = 3
+
+    FlowRow(
+        modifier = Modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        maxItemsInEachRow = rows
+    ) {
+        val itemModifier = Modifier
+            .padding(4.dp)
+            .height(80.dp)
+            .weight(1f)
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.Blue)
+
+        repeat(rows * columns) {
+            Spacer(modifier = itemModifier)
+        }
+    }
+}
